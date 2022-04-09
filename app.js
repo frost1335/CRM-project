42ff442d9720c16f861becbe6c82ee4bd3570103
raximvodil <raximvodilrozbek@gmail.com>
2022-04-09 16:03:46 +0500

    som edits

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
 
 // Connect DB
 connectDB();
@@ -9,6 +10,12 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  console.req
+  next();
+});
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/private", require("./routes/private"));
